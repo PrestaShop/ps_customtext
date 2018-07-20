2fc6fed900971410938592c23bcf62f23a9664da
Thomas N <thomas.nabord@prestashop.com>
2018-07-20 11:25:10 +0200

    // Version updated to v4.1.0

--- ps_customtext.php
+++ ps_customtext.php
@@ -44,7 +44,7 @@ class Ps_Customtext extends Module implements WidgetInterface
     {
         $this->name = 'ps_customtext';
         $this->author = 'PrestaShop';
-        $this->version = '4.0.0';
+        $this->version = '4.1.0';
         $this->need_instance = 0;
 
         $this->bootstrap = true;
