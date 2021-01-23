589ee85226e4cc1baf0a77d4950e6ccb20910bf9
Moncef Essid <moncef102@gmail.com>
2021-01-23 21:06:41 -0100

    use PHP_EOL instead of \n
    use PHP_EOL instead of \n

--- ps_customtext.php
+++ ps_customtext.php
@@ -292,9 +292,9 @@ class Ps_Customtext extends Module implements WidgetInterface
         $return = true;
         $tabTexts = [
             [
-                'text' => '<h2>Custom Text Block</h2>
-<p><strong class="dark">Lorem ipsum dolor sit amet conse ctetu</strong></p>
-<p>Sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.</p>',
+                'text' => '<h2>Custom Text Block</h2>'.PHP_EOL.
+                    '<p><strong class="dark">Lorem ipsum dolor sit amet conse ctetu</strong></p>'.PHP_EOL.
+                    '<p>Sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.</p>',
             ],
         ];
 
